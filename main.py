523316ab4d430dc74a1a18463e428ba526ceb620
umsyt <mxcheeks@gmail.com>
2020-11-28 21:41:47 +0000

    cooler

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-h = input("bruh")
-print(h+h)
+fates = ["money", "love", "happiness", "hardship", "peace"]
+
+r = int(input("What is the date of your birthday? "))
+
+print("You will encounter "+fates[r%5])
